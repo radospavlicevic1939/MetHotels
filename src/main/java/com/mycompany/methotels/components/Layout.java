605559fb4d66b55416ffc67e79dc2e1e3057b5fc
rados.pavlicevic <rados.pavlicevic.1939@metropolitan.ac.rs>
2015-04-18 00:47:48 +0200

    IT250-DZ07-Rados_Pavlicevic_1939

--- src/main/java/com/mycompany/methotels/components/Layout.java
+++ src/main/java/com/mycompany/methotels/components/Layout.java
@@ -5,7 +5,6 @@ import org.apache.tapestry5.annotations.*;
 /**
  * Layout component for pages of application test-project.
  */
-@Import(module="bootstrap/collapse", stylesheet = "context:css/style.css")
-public class Layout
-{
+@Import(module = "bootstrap/collapse", stylesheet = "context:css/style.css")
+public class Layout {
 }
